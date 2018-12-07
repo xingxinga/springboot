bea3b93d1a5b0909ba0c4464efec143d7b1f40a5
lixing <xing.li@chsoft.com.cn>
2018-12-07 16:20:53 +0800

    delete

--- src/main/java/com/chsoft/sys/BaseEntity.java
+++
@@ -1,9 +0,0 @@
-package com.chsoft.sys;
-
-import java.io.Serializable;
-
-public class BaseEntity implements Serializable {
-    
-    private static final long serialVersionUID = 218433646468062289L;
-
-}
