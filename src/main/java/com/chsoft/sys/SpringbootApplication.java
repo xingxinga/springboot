03146a393663339a9e51f1a9b545d9d99302b774
lixing <xing.li@chsoft.com.cn>
2018-12-17 11:59:50 +0800

    发版修改文件

--- src/main/java/com/chsoft/sys/SpringbootApplication.java
+++ src/main/java/com/chsoft/sys/SpringbootApplication.java
@@ -3,11 +3,25 @@ package com.chsoft.sys;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.chsoft")
 @ComponentScan("com")
+/*public class SpringbootApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+
+		return application.sources(SpringbootApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+}*/
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
